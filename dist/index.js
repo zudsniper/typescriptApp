9ac4108cb0c374f677e2f248c0b010a74424b13e
jason <jason.p.mcelhenney@gmail.com>
2023-01-15 15:26:55 -0600

    removing target from repo

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * index.ts
- * @description - Entry point for the application
- * @author - zudsniper@github
- */
-require('dotenv').config();
-const chalk = require('chalk');
-//# sourceMappingURL=index.js.map
