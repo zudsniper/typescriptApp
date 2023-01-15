fccaa6f3b582cf5cd34c350da13b63551f2fb6d1
jason <jason.p.mcelhenney@gmail.com>
2023-01-15 09:22:09 -0600

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+/**
+ * index.ts
+ * @description - Entry point for the application
+ * @author - zudsniper@github
+ */
+require('dotenv').config();
+const chalk = require('chalk');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * index.ts
+ * @description - Entry point for the application
+ * @author - zudsniper@github
+ * created 1/15/2023
+ */
+
+require('dotenv').config();
+
+const chalk = require('chalk');
